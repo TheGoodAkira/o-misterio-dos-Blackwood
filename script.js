06abd2b2086286749b3c1741a65e5fde16163e15
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2024-09-06 21:13:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,26 +9,3 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    function mostrarMensagemFinal() {
-      const mensagemFinal = document.getElementById('mensagem-final');
-      mensagemFinal.style.display = 'block';
-    }
-  
-    function verificarFinal() {
-      const passoAtual = document.querySelector('.passo.ativo');
-      if (passoAtual && passoAtual.id === 'passo-40') {
-        mostrarMensagemFinal();
-      }
-    }
-  
-    const botoes = document.querySelectorAll('.btn-proximo');
-    botoes.forEach(botao => {
-      botao.addEventListener('click', () => {
-
-        verificarFinal();
-      });
-    });
-  });
-  
